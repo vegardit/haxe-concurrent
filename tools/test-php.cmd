38885dbb22660600eda061ce2031329aa2e4f908
sebthom <sebthom@users.noreply.github.com>
2021-08-01 14:03:26 +0200

    update tools

---
+++ tools/test-php.cmd
@@ -0,0 +1,15 @@
+@echo off
+REM Copyright (c) 2016-2021 Vegard IT GmbH (https://vegardit.com) and contributors.
+REM SPDX-License-Identifier: Apache-2.0
+REM Author: Sebastian Thomschke, Vegard IT GmbH
+
+call %~dp0_test-prepare.cmd php
+
+echo Compiling...
+haxe %~dp0..\tests.hxml -php target\php
+set rc=%errorlevel%
+popd
+if not %rc% == 0 exit /b %rc%
+
+echo Testing...
+%PHP7_HOME%\php "%~dp0..\target\php\index.php"
